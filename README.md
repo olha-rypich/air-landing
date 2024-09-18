3acbad77065d6cd83d5b68fdb56aadbed739298d
Olha Rypich <olha.rypich@gmail.com>
2024-09-18 15:22:15 +0300

    air-landing-page

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+# Air Landing Page
+
+The aim of this project is to present the creative commercial agency as a company with innovative strategies, fresh ideas, a wide range of services, and skilled staff.
+
+The landing page comprises a number of sections, namely:
+- a header with a navigation menu;
+- the introductory section with a slider;
+- three sections presenting the objectives of the company and its services;
+- the "testimonials" section introducing the senior staff;
+- a contact section with a form and contact information;
+- a footer with useful links.
+
+## Design
+
+The project was implemented according to [Figma design](https://www.figma.com/file/7qwsWggv9BAxMi2VPhBuPr/Air-(formerly-Dia)?node-id=9138%3A35).
+
+The mockup is complemented by the adaptive slider that can be expanded to any number of slides if necessary.
+
+## Technologies & Tools Used
+- **HTML**
+- **BEM**
+- **SASS**
+- **JavaScript**
+- **Responsive Design**
+
+## Demo
+A live demo of the Air Landing is available at [DEMO](https://olha-rypich.github.io/air-landing/).
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,75 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.send-us-a-message__form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  form.reset();
+});
+
+const images = document.querySelectorAll('.slider__image');
+const sliderLine = document.querySelector('.slider__line');
+const buttonLeft = document.querySelector('.slider__button-left');
+const buttonRight = document.querySelector('.slider__button-right');
+const styles = window.getComputedStyle(buttonLeft);
+const initPosition = styles.getPropertyValue('left');
+let count = 0;
+let width = 0;
+
+function init() {
+  width = document.querySelector('.slider').offsetWidth;
+  sliderLine.style.width = width * images.length + 'px';
+
+  images.forEach(item => {
+    item.style.width = width + 'px';
+  });
+
+  rollSlider();
+}
+
+window.addEventListener('resize', init);
+
+init();
+
+buttonLeft.addEventListener('click', function() {
+  count--;
+  buttonRight.style.opacity = '1';
+
+  if (count < 0) {
+    count = 0;
+  }
+
+  if (count === 0) {
+    buttonRight.style.left = initPosition;
+  }
+
+  rollSlider();
+});
+
+buttonRight.addEventListener('click', function() {
+  count++;
+  buttonRight.style.left = +initPosition.slice(0, 2) + 48 + 'px';
+
+  if (count >= images.length) {
+    count = images.length - 1;
+  }
+
+  if (count === images.length - 1) {
+    buttonRight.style.opacity = '0';
+  }
+
+  rollSlider();
+});
+
+function rollSlider() {
+  sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+}
